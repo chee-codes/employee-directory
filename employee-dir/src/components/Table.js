102569b9e48c53333643364ca0bdf9b2d3e39736
Jaja Brown <65190496+jbrown827@users.noreply.github.com>
2021-02-25 01:22:54 -0600

    Update package.json

--- employee-dir/src/components/Table.js
+++ employee-dir/src/components/Table.js
@@ -1,4 +1,5 @@
 import React from "react";
+import TableBody from "./TableBody";
 
 const Table = () => {
   return (
@@ -12,26 +13,7 @@ const Table = () => {
             <th scope="col">Handle</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
+        <TableBody />
       </table>
     </div>
   );
